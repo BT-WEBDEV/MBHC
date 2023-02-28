293778cea14c5d7ed6a4aa81fab0976b2d4a9eb3
Brandon Tetrick <brandontetrick@gmail.com>
2023-02-28 08:58:13 -0700

    added new pages

--- gka-theme-widgets/widgets/gka-theme-features/tpl/default.php
+++ gka-theme-widgets/widgets/gka-theme-features/tpl/default.php
@@ -43,5 +43,46 @@
                 </div>
             </div>
         </div>
+        <div class="row mt-5">
+            <div class="col-lg-6">
+                <h1 class="text-l-blue">Additional Features</h1>
+                <div class="feature-list">
+                    <div class="image z-depth-1-half">
+                        <img src="<?php echo get_template_directory_uri(). "/images/special-features-1-sm.jpg" ?>" alt=""
+                            class="img-fluid">
+                    </div>
+                    <div class="content text-white text-center">
+                        <h4>Accessible Home Building</h3>
+                            <hr>
+                            <p>
+                            Mitchell & Best is proud to offer accessibility options for all our homeowners. 
+                            Work with our designers and builders to create a home customized to your needs. 
+                            Our floor plans and features can support aging in place, wheelchair accessibility, and mobility accommodations. 
+                            Reach out to learn more about our accessible homes and collaborative building process!
+                            </p>
+                            <a href="/accessible-home-building" class="btn custom-btn text-white">MORE INFO</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="feature-list">
+                    <div class="image z-depth-1-half">
+                        <img src="<?php echo get_template_directory_uri(). "/images/special-features-2-sm.jpg" ?>" alt=""
+                            class="img-fluid">
+                    </div>
+                    <div class="content text-white text-center">
+                        <h4>Multigenerational Homes</h3>
+                            <hr>
+                            <p>
+                            Our Hometown Collection supports multigenerational living. 
+                            Customize your floor plan to suit your entire family, whether you’re raising young children or aging in place. 
+                            Our universal design offers options for dual owner’s suites, private living spaces, and specialized accessibility. 
+                            Contact us to learn more about how we can build your family’s unique home!
+                            </p>
+                            <a href="/multigenerational-homes" class="btn custom-btn text-white">MORE INFO</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
